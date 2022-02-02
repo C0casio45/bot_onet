b2363600ed2e0da2a3c769f59e2917b0da780a40
BuildTools <sarquentin@gmail.com>
2022-02-02 16:06:48 +0100

    Adding private message button's utils

---
+++ button/mp_loop_close.js
@@ -0,0 +1,20 @@
+const { MessageActionRow, MessageButton } = require("discord.js");
+
+module.exports = {
+  name: "mp_loop_button_close",
+  description: "",
+  execute: function (interaction, client) {
+    interaction.channel.send({ content: "n" });
+
+    const row = new MessageActionRow().addComponents(
+      new MessageButton()
+        .setCustomId("done")
+        .setLabel(`Fermer`)
+        .setStyle("SUCCESS")
+        .setEmoji("❌")
+        .setDisabled("true")
+    );
+
+    interaction.update({ components: [row] });
+  },
+};
---
+++ button/mp_loop_continue.js
@@ -0,0 +1,20 @@
+const { MessageActionRow, MessageButton } = require("discord.js");
+
+module.exports = {
+  name: "mp_loop_button_continue",
+  description: "",
+  execute: function (interaction, client) {
+    interaction.channel.send({ content: "o" });
+
+    const row = new MessageActionRow().addComponents(
+      new MessageButton()
+        .setCustomId("done")
+        .setLabel(`Continuer`)
+        .setStyle("SUCCESS")
+        .setEmoji("➕")
+        .setDisabled("true")
+    );
+
+    interaction.update({ components: [row] });
+  },
+};
