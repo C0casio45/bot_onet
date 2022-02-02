5bb8c5f8c4157cb8196ee54c99e7d8233624a67d
BuildTools <sarquentin@gmail.com>
2022-02-02 15:41:03 +0100

    Adding buttons utilities

---
+++ button/avertissement.js
@@ -0,0 +1,20 @@
+const { MessageActionRow, MessageButton } = require("discord.js");
+
+module.exports = {
+  name: "avertissement",
+  description: "Méthode pour créer un avertissement",
+  execute: function (interaction, client) {
+    interaction.channel.send({ content: "0" });
+
+    const row = new MessageActionRow().addComponents(
+      new MessageButton()
+        .setCustomId("done")
+        .setLabel(`Avertissement`)
+        .setStyle("SUCCESS")
+        .setEmoji("🚨")
+        .setDisabled("true")
+    );
+
+    interaction.update({ components: [row] });
+  },
+};
