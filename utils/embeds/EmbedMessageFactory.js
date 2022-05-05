0293f6c8992afdcb5a13d9fe805522bc76b9ca55
TOM LUCAS <tom.lucas@viacesi.fr>
2022-05-05 13:59:52 +0200

    :art: setup Message factory

---
+++ utils/embeds/EmbedMessageFactory.js
@@ -0,0 +1,25 @@
+class Message {
+    constructor(content) {
+        this.description = content;
+        this.color = "#e34c3b";
+        this.author = "Utilitaire de banissement";
+    }
+    setColor(color) {
+        this.color = color;
+        return this;
+    }
+    setAuthor(author) {
+        this.author = author;
+        return this;
+    }
+    get embed() {
+        new MessageEmbed()
+        .setColor(this.color)
+        .setAuthor({ name: this.author })
+        .setDescription(this.description)
+        .setFooter({ text: "Créé et hébergé par COcasio45#2406" })
+        .setTimestamp();
+    }
+    
+}
+module.exports = Message;
