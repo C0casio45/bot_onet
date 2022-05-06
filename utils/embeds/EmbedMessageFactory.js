4be6e42c8cab13fb264a4bd0c000a9846d400d57
TOM LUCAS <tom.lucas@viacesi.fr>
2022-05-06 19:16:21 +0200

    :art: add error code

--- utils/embeds/EmbedMessageFactory.js
+++ utils/embeds/EmbedMessageFactory.js
@@ -6,20 +6,42 @@ class Message {
         this.author = author;
         this.color = color;
     }
-    error(){
+
+    error(code){
+        switch (code) {
+            case 1:
+              this.author = `Vous avez mis trop de temps a répondre, merci de recommencer la démarche en écrivant /ban [ticket]`;
+              break;
+            case 2:
+              this.author = `Merci de relancer une demande d'unban en indiquant un numéro la prochaine fois`;
+              break;
+        
+        
+            default:
+              this.author = `Erreur inconnue`;
+              break;
+          }
         this.setColor("#F58B00");
     }
+
     success(){
         this.setColor("#00ff00");
     }
+
+    rappelUnban(){
+        this.setAuthor("Rappel de débannissement");
+    }
+
     setColor(color) {
         this.color = color;
         return this;
     }
+
     setAuthor(author) {
         this.author = author;
         return this;
     }
+
     get embed() {
         return new MessageEmbed()
             .setColor(this.color)
@@ -28,6 +50,7 @@ class Message {
             .setFooter({ text: "Créé et hébergé par COcasio45#2406" })
             .setTimestamp();
     }
+    
     get raw() {
         return this.author + ": " + this.description;
     }
