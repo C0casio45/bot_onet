cb2e89b27a954182cb7a63dd25e520810950ccf6
TOM LUCAS <tom.lucas@viacesi.fr>
2022-05-09 14:13:35 +0200

    :fire: delete old method

--- bot_modules/sendFunction.js
+++ bot_modules/sendFunction.js
@@ -1,4 +1,3 @@
-const { MessageActionRow, MessageButton, MessageEmbed } = require("discord.js");
 const con = require("../dbconnect.js");
 const db = con.database();
 const faceit = require("./faceit.js");
@@ -98,18 +97,4 @@ function sendEmbed(pseudo, duree, date) {
   let content = `Le joueur **${pseudo}** a été banni pour une durée de **${duree} jours** le ${date}.
   Il a été débanni a ce jour.`
   return new Message(content, "Rappel unban").embed();
-  return new MessageEmbed()
-    .setColor("#e34c3b")
-    .setAuthor({ name: "Rappel unban" })
-    .setDescription(
-      `Le joueur **${pseudo}** a été banni pour une durée de **${duree} jours** le ${date}.
-        Il a été débanni a ce jour.`
-    )
-    .addField(
-      "Lien vers le pannel faceit de banissement",
-      "https://www.faceit.com/fr/hub/f3150918-521a-4664-b430-4e4713b91495/OneT%20Community/admin/tickets",
-      false
-    )
-    .setFooter({ text: "Créé et hébergé par COcasio45#2406" })
-    .setTimestamp();
 }
