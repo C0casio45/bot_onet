ca7bf63eb6a128bee545fd005f4b4757e5ed816c
TOM LUCAS <tom.lucas@viacesi.fr>
2022-05-10 14:38:42 +0200

    :art: improve code format

--- commands/ban.js
+++ commands/ban.js
@@ -1,10 +1,8 @@
-const con = require("../utils/dbconnext.js");
-const db = con.database();
+const db = require("../utils/db/dbLibrary.js");
 const dp = require(`../bot_modules/deploy.js`);
 
 const { mp_sanction_buttons } = require("../utils/buttons/mp_sanction_buttons");
 const { mp_loop_buttons } = require("../utils/buttons/mp_loop_buttons");
-const { send_ban } = require("../utils/embeds/send_ban.js");
 const faceit = require("../bot_modules/faceit.js");
 
 const Message = require("../utils/embeds/MessagesLibrary");
@@ -174,51 +172,7 @@ module.exports = {
               ) {
                 quiz(i + 1);
               } else {
-                if (!db._connectCalled) {
-                  db.connect();
-                }
-
-                //load data in database
-                array.forEach((row) => {
-                  // id_Ticket, pseudo_accusé, Lien_Accusé, Lien_Partie, Duree_jours, raison, Fermé?
-                  db.query(
-                    `call bot_onet.close_ticket(${options}, '${row[0]}', 'https://www.faceit.com/fr/players/${row[0]}', '${liengame}', ${row[1]}, '${row[2]}', TRUE);`,
-                    function (err) {
-                      console.log(err);
-                      if (err) throw err;
-                    }
-                  );
-                  if (!row[1] == 0) {
-                    //ban player in faceit
-                    faceit.BanPlayer(
-                      row[0],
-                      "Ban " +
-                      (row[1] == 99999 ? "perm" : row[1] + "j") +
-                      ". Plus d'informations sur notre discord.",
-                      (failed, error = null) => {
-                        if (failed)
-                          rmsg.channel.send({
-                            embeds: [Message.error(`${error}`)],
-                          });
-                        else
-                          rmsg.channel.send({
-                            embeds: [
-                              Message.success("Ticket fermé avec succès."),
-                            ],
-                          });
-                      }
-                    );
-                  }
-                });
-
-                //send message in private to user who banned the player
-                //rmsg.channel.send({embeds : [send_ban(array.length,array)]});
-                //send message in discord channel
-                ban.send({
-                  embeds: [send_ban(array.length, array, userid, unban)],
-                });
-                //update discord cache
-                dp.dply(client, "0", interaction.guildId);
+                closeTickets(liengame);
               }
             })
             .catch((err) => {
@@ -226,6 +180,45 @@ module.exports = {
               rmsg.channel.send({ embeds: [Message.error(1)] });
             });
         });
+    }
+
+    function closeTickets(liengame) {
+      //load data in database
+      array.forEach((row) => {
+        // id_Ticket, pseudo_accusé, Lien_Accusé, Lien_Partie, Duree_jours, raison, Fermé?
+        db.closeTicket(options, row[0], 'https://www.faceit.com/fr/players/${row[0]}', liengame, row[1], row[2]);
+
+        if (!row[1] == 0) {
+          //ban player in faceit
+          faceit.BanPlayer(
+            row[0],
+            "Ban " +
+            (row[1] == 99999 ? "perm" : row[1] + "j") +
+            ". Plus d'informations sur notre discord.",
+            (failed, error = null) => {
+              if (failed)
+                rmsg.channel.send({
+                  embeds: [Message.error(`${error}`)],
+                });
+              else
+                rmsg.channel.send({
+                  embeds: [
+                    Message.success("Ticket fermé avec succès."),
+                  ],
+                });
+            }
+          );
+        }
+      });
+
+      //send message in private to user who banned the player
+      //rmsg.channel.send({embeds : [Message.banLog(array.length,array)]});
+      //send message in discord channel
+      ban.send({
+        embeds: [Message.banLog(array.length, array, userid, unban)],
+      });
+      //update discord cache
+      dp.dply(client, "0", interaction.guildId);
     }
   },
 };
