a9dc4c6371e1b62f5d17c4f347cc3f1d32033ddd
Tom <tom.lucas@viacesi.fr>
2022-05-11 18:39:45 +0200

    🐛 fix deploy before sql update

--- bot_modules/deploy.js
+++ bot_modules/deploy.js
@@ -1,100 +1,102 @@
 const db = require("../utils/db/dbLibrary.js");
 
 module.exports = {
-    dply: async function (client, p, id) {
+    dply: function (client, p, id) {
+        return new Promise(async (resolve, reject) => {
+            id = id == undefined ? "870319455115284481" : id;
 
-        id = id == undefined ? "870319455115284481" : id;
+            switch (p) {
+                case "0":
+                    const tickets = await db.getTicketList()
+                    const user = await db.getBannedList()
 
-        switch (p) {
-            case "0":
-                const tickets = await db.getTicketList()
-                const user = await db.getBannedList()
+                    const dataGuild = [{
+                        "name": 'ban',
+                        "description": "Méthode pour bannir les gens",
+                        "options": [
+                            {
+                                "name": 'ticket',
+                                "description": 'Nom du ticket',
+                                "type": 'STRING',
+                                "required": true,
+                                "choices": tickets
+                            }
+                        ]
+                    }];
 
-                const dataGuild = [{
-                    "name": 'ban',
-                    "description": "Méthode pour bannir les gens",
-                    "options": [
-                        {
-                            "name": 'ticket',
-                            "description": 'Nom du ticket',
-                            "type": 'STRING',
-                            "required": true,
-                            "choices": tickets
-                        }
-                    ]
-                }];
+                    dataGuild.push({
+                        "name": 'close',
+                        "description": "Méthode fermer les tickets",
+                        "options": [
+                            {
+                                "name": 'ticket',
+                                "description": 'Nom du ticket',
+                                "type": 'STRING',
+                                "required": true,
+                                "choices": tickets
+                            }
+                        ]
+                    });
 
-                dataGuild.push({
-                    "name": 'close',
-                    "description": "Méthode fermer les tickets",
-                    "options": [
-                        {
-                            "name": 'ticket',
-                            "description": 'Nom du ticket',
-                            "type": 'STRING',
-                            "required": true,
-                            "choices": tickets
-                        }
-                    ]
-                });
+                    dataGuild.push({
+                        "name": 'unban',
+                        "description": "Méthode débannir des gens",
+                        "options": [
+                            {
+                                "name": "utilisateur",
+                                "description": "Pseudo de l'utilisateur a bannir",
+                                "type": "STRING",
+                                "required": true,
+                                "choices": user
+                            }
+                        ]
+                    });
 
-                dataGuild.push({
-                    "name": 'unban',
-                    "description": "Méthode débannir des gens",
-                    "options": [
-                        {
-                            "name": "utilisateur",
-                            "description": "Pseudo de l'utilisateur a bannir",
-                            "type": "STRING",
-                            "required": true,
-                            "choices": user
-                        }
-                    ]
-                });
+                    resolve(client.guilds.cache.get(id)?.commands.set(dataGuild));
+                    break;
+                case "1":
+                    const banned = await db.getBannedList()
+                    resolve(banned);
+                    break;
+                case "2":
+                    const dataGlobal = [{
+                        name: 'take',
+                        description: "Méthode pour prendre les tickets",
+                        options: [
+                            {
+                                name: 'ticket',
+                                description: 'Nom du ticket',
+                                type: 'CHANNEL',
+                                required: true
+                            }
+                        ]
+                    },
+                    {
+                        name: 'stats',
+                        description: "Méthode pour voir les statistiques des modérateurs",
+                        options: [
+                            {
+                                "name": "moderateur",
+                                "description": "Pseudo du modérateur",
+                                "type": "USER",
+                                "required": false
+                            }
+                        ]
+                    },
+                    {
+                        name: 'send',
+                        description: "Méthode vérifier les joueurs a unban du jour"
+                    }];
 
-                client.guilds.cache.get(id)?.commands.set(dataGuild);
-                break;
-            case "1":
-                const banned = await db.getBannedList()
-                console.log(banned);
-                break;
-            case "2":
-                const dataGlobal = [{
-                    name: 'take',
-                    description: "Méthode pour prendre les tickets",
-                    options: [
-                        {
-                            name: 'ticket',
-                            description: 'Nom du ticket',
-                            type: 'CHANNEL',
-                            required: true
-                        }
-                    ]
-                },
-                {
-                    name: 'stats',
-                    description: "Méthode pour voir les statistiques des modérateurs",
-                    options: [
-                        {
-                            "name": "moderateur",
-                            "description": "Pseudo du modérateur",
-                            "type": "USER",
-                            "required": false
-                        }
-                    ]
-                },
-                {
-                    name: 'send',
-                    description: "Méthode vérifier les joueurs a unban du jour"
-                }];
+                    resolve(client.application.commands?.set(dataGlobal));
+                    break;
 
-                client.application.commands?.set(dataGlobal);
-                console.log("2 is done !")
-                break;
+                default:
+                    reject();
+                    break;
 
-            default:
-                break;
-        }
+            }
+        });
         //client.api.applications(client.user.id).commands("command-id").delete(); 
         //const commands = await client.guilds.cache.get('870319455115284481')?.commands.fetch();
         //const commands = await client.application.commands?.fetch();
